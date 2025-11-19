d9b6032a97a2685fd1557a34044b6c5b8f244086
Ckyever Gaviola <ckyeverg@gmail.com>
2025-11-20 10:57:32 +1100

    Show a more readable format for message timestamp

--- models/messasges.js
+++ models/messasges.js
@@ -1,18 +1,37 @@
+const getCurrentUTCDateTime = () => {
+  return new Date().toISOString();
+};
+
 const messages = [
   {
-    text: "First!",
+    text: "Last year",
     user: "ckyzo",
-    added: new Date(),
+    added: "2024-07-01T23:42:13.787Z",
   },
   {
-    text: "Second?!",
+    text: "Last month",
     user: "chanpan",
-    added: new Date(),
+    added: "2025-10-01T11:02:53.787Z",
+  },
+  {
+    text: "Yesterday",
+    user: "ninajirachi",
+    added: "2025-11-18T23:42:13.787Z",
+  },
+  {
+    text: "An hour ago",
+    user: "fike",
+    added: "2025-11-19T22:42:13.787Z",
+  },
+  {
+    text: "Just then",
+    user: "garchomp",
+    added: getCurrentUTCDateTime(),
   },
 ];
 
 function postMessage(text, username) {
-  messages.push({ text: text, user: username, added: new Date() });
+  messages.push({ text: text, user: username, added: getCurrentUTCDateTime() });
 }
 
 export { messages, postMessage };
--- routes/chatRouter.js
+++ routes/chatRouter.js
@@ -2,10 +2,25 @@ import { Router } from "express";
 import { messages, postMessage } from "../models/messasges.js";
 import user from "../models/user.js";
 
+const getChatTimestamp = (utcDateTime) => {
+  return new Date(utcDateTime).toLocaleString("en-AU", {
+    weekday: "short",
+    month: "short",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+  });
+};
+
 const chatRouter = Router();
 
 chatRouter.get("/", (req, res) => {
-  res.render("chat", { username: user.username, messages: messages });
+  res.render("chat", {
+    username: user.username,
+    messages: messages,
+    getChatTimestamp: getChatTimestamp,
+  });
 });
 
 chatRouter.post("/new", (req, res) => {
