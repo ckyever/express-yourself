fd009f655fb50b9cc502e21a915178c9b3940f9f
Ckyever Gaviola <ckyeverg@gmail.com>
2025-11-19 17:24:35 +1100

    Add the ability to create a post

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const app = express();
 app.set("views", path.join(currentDirectory, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/", indexRouter);
 
 const PORT = 3000;
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -19,4 +19,17 @@ indexRouter.get("/", (req, res) => {
   res.render("index", { messages: messages });
 });
 
+indexRouter.get("/new", (req, res) => {
+  res.render("form");
+});
+
+indexRouter.post("/new", (req, res) => {
+  messages.push({
+    text: req.body.messageText,
+    user: req.body.username,
+    added: new Date(),
+  });
+  res.redirect("/");
+});
+
 export { indexRouter };
