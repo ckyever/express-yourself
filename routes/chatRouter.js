1520c59159b78e2bcb36aa5797d77abf90a715a3
Ckyever Gaviola <ckyeverg@gmail.com>
2025-11-20 17:20:06 +1100

    Show today if message was sent today locale time

--- routes/chatRouter.js
+++ routes/chatRouter.js
@@ -2,16 +2,44 @@ import { Router } from "express";
 import { messages, postMessage } from "../models/messasges.js";
 import user from "../models/user.js";
 
+const getDayMonthYearFromUtcDateTime = (utcDateTime = null) => {
+  if (utcDateTime == null) {
+    return new Date().toLocaleString("en-AU", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "2-digit",
+    });
+  } else {
+    return new Date(utcDateTime).toLocaleString("en-AU", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "2-digit",
+    });
+  }
+};
+
 const getChatTimestamp = (utcDateTime) => {
-  return new Date(utcDateTime).toLocaleString("en-AU", {
-    weekday: "short",
-    day: "numeric",
-    month: "short",
-    year: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
-    hour12: true,
-  });
+  const chatDate = getDayMonthYearFromUtcDateTime(utcDateTime);
+  const currentDate = getDayMonthYearFromUtcDateTime();
+
+  if (chatDate === currentDate) {
+    const time = new Date(utcDateTime).toLocaleString("en-AU", {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+    });
+    return "Today, " + time;
+  } else {
+    return new Date(utcDateTime).toLocaleString("en-AU", {
+      weekday: "short",
+      day: "numeric",
+      month: "short",
+      year: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+    });
+  }
 };
 
 const chatRouter = Router();
